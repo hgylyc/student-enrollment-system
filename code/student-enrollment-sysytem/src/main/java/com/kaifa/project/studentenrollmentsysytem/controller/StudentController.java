711e1cbe0d98c22c8810e124d185e5b29bb8d0b6
Xia <3406144972@qq.com>
2024-07-02 22:59:17 +0800

    InformationQueryController

--- code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/controller/StudentController.java
+++
@@ -1,8 +0,0 @@
-package com.kaifa.project.studentenrollmentsysytem.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class StudentController {
-}
--- code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/service/Impl/TeacherServiceImpl.java
+++ code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/service/Impl/TeacherServiceImpl.java
@@ -1,11 +1,39 @@
 package com.kaifa.project.studentenrollmentsysytem.service.Impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.kaifa.project.studentenrollmentsysytem.pojo.Teacher;
 import com.kaifa.project.studentenrollmentsysytem.mapper.TeacherMapper;
 import com.kaifa.project.studentenrollmentsysytem.service.TeacherService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> implements TeacherService {
+
+    @Override
+    public Teacher getTeacherByName(String teacherName) {
+        return baseMapper.selectOne(new QueryWrapper<Teacher>().eq("teacher_name", teacherName));
+    }
+
+    @Override
+    public List<Teacher> getAllTeachers() {
+        return baseMapper.selectList(null);
+    }
+
+    @Override
+    public boolean addTeacher(Teacher teacher) {
+        return baseMapper.insert(teacher) > 0;
+    }
+
+    @Override
+    public boolean updateTeacher(Teacher teacher) {
+        return baseMapper.updateById(teacher) > 0;
+    }
+
+    @Override
+    public boolean deleteTeacherById(String teacherId) {
+        return baseMapper.deleteById(teacherId) > 0;
+    }
 }
