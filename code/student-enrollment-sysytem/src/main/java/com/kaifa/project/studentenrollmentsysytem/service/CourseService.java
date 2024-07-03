b52e3458f4b5a0832b2966bb62f9fb722ae25946
Xia <3406144972@qq.com>
2024-07-03 11:32:24 +0800

    CourseService函数
    Signed-off-by: Xia <3406144972@qq.com>

--- code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/service/CourseService.java
+++ code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/service/CourseService.java
@@ -2,6 +2,11 @@ package com.kaifa.project.studentenrollmentsysytem.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.kaifa.project.studentenrollmentsysytem.pojo.Course;
+import java.util.List;
 
 public interface CourseService extends IService <Course>{
+    List<Course> getCoursesByStudentAcademy(String studentId);
+    boolean isCourseFull(String courseId);
+    void incrementCurrentNumOfStu(String courseId);
+    List<Course> filterCourses(String studentId, String courseType, String courseName);
 }
--- code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/service/Impl/CourseServiceImpl.java
+++ code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/service/Impl/CourseServiceImpl.java
@@ -1,12 +1,41 @@
 package com.kaifa.project.studentenrollmentsysytem.service.Impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.kaifa.project.studentenrollmentsysytem.mapper.CourseMapper;
+import com.kaifa.project.studentenrollmentsysytem.mapper.StudentMapper;
 import com.kaifa.project.studentenrollmentsysytem.pojo.Course;
+import com.kaifa.project.studentenrollmentsysytem.pojo.Student;
 import com.kaifa.project.studentenrollmentsysytem.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CourseServiceImpl extends ServiceImpl <CourseMapper, Course> implements CourseService{
+    @Autowired
+    private StudentMapper studentMapper;
+    @Override
+    public List<Course> getCoursesByStudentAcademy(String studentId) {
+        Student student = studentMapper.selectById(studentId);
+        String academy = student.getAcademy();
+        return baseMapper.selectList(new QueryWrapper<Course>().likeRight("course_id", academy));
+    }
+
+    @Override
+    public boolean isCourseFull(String courseId) {
+        return false;
+    }
+
+    @Override
+    public void incrementCurrentNumOfStu(String courseId) {
+
+    }
+
+    @Override
+    public List<Course> filterCourses(String studentId, String courseType, String courseName) {
+        return null;
+    }
 }
 
