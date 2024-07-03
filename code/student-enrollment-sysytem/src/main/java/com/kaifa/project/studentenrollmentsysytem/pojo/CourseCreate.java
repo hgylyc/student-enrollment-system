f8a556028aa2a58a2f4bf3cda1dc5f96e4ed36ab
lyc <2637927098@qq.com>
2024-07-03 11:20:34 +0800

    添加了俩controller

---
+++ code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/pojo/CourseCreate.java
@@ -0,0 +1,19 @@
+package com.kaifa.project.studentenrollmentsysytem.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+@Data
+public class CourseCreate {
+    public String courseName;   // 课程名
+    public String teacherName; // 教师姓名
+    public String teacherId; // 教师工号
+    public int ceilingOfPersonnel; // 人数上限
+    public String courseType; // 课程类型
+    public String score; // 学分
+    public String semester; //开课学期
+    public String institution; //开课学院
+    public String identificationCode;  //四位标识码
+    public String introduction; // 课程简介
+}
