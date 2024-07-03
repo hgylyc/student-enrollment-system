a0664890fa6d59ecad70037e29c97c4fa90cdd8a
yyk <3240887094@qq.com>
2024-07-03 16:46:19 +0800

    更新管理stumapper和stuservice

---
+++ code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/controller/InformController.java
@@ -0,0 +1,19 @@
+package com.kaifa.project.studentenrollmentsysytem.controller;
+
+import com.kaifa.project.studentenrollmentsysytem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/systemInform")
+public class InformController {
+    @Autowired
+    private StudentService studentService;
+    @GetMapping("/InformContent/{stuId}")
+    public List<Map<String,Object>> stuProcessState(@PathVariable String stuId) {
+        return studentService.selectStateById(stuId);
+    }
+}
