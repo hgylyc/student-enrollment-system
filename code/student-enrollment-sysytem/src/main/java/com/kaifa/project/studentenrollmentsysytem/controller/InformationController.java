606f0feebd9b77826eedcb5d5f002cfaaaa95dce
eee <884473883@qq.com>
2024-07-03 16:24:20 +0800

    informationcontroller

--- code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/controller/InformationController.java
+++ code/student-enrollment-sysytem/src/main/java/com/kaifa/project/studentenrollmentsysytem/controller/InformationController.java
@@ -19,18 +19,18 @@ public class InformationController {
     @Autowired
     private StudentService studentService;
 
-//    @PostMapping("/information/dormitories")
-//    public List<Dormitory> getDormitoriesByStudentId(HttpSession session){
-//        String studentId = (String) session.getAttribute("studentId");
-//        Student student = studentService.getStudentById(studentId);
-//        // 获取该专业和性别对应的所有宿舍
-//        if (studentId != null) {
-//            String academy = student.getAcademy();
-//            String gender = student.getGender();
-//            return studentService.getDormitoriesByMajorAndGender(major, gender);
-//        }
-//
-//        // 如果学生未找到，返回空列表或抛出异常
-//        return List.of();
-//    }
+    @PostMapping("/information/dormitories")
+    public List<Dormitory> getDormitoriesByStudentId(HttpSession session){
+        String studentId = (String) session.getAttribute("studentId");
+        Student student = studentService.getStudentById(studentId);
+        // 获取该专业和性别对应的所有宿舍
+        if (studentId != null) {
+            String academy = student.getAcademy();
+            String gender = student.getGender();
+            return studentService.getDormByAcGender(academy, gender);
+        }
+
+        // 如果学生未找到，返回空列表或抛出异常
+        return null;
+    }
 }
